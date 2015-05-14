4b840ed55ac3eac0e9fdc553068d092c6e18d73c
aalpern <adam.alpern@gmail.com>
2015-05-14 07:55:17 -0700

    License & commentary

--- color-theme-autumn-light.el
+++ color-theme-autumn-light.el
@@ -1,4 +1,5 @@
 ;;; color-theme-autumn-light.el -- A light color theme with muted, autumnal colors.
+;;;
 ;; Copyright 2015 Adam Alpern
 ;;
 ;; Author: Adam Alpern <adam.alpern@gmail.com>
@@ -7,6 +8,34 @@
 ;; Keywords: color, theme
 ;; Version: 1.0.0
 ;; Package-Requires: ((color-theme "6.5.5"))
+;;
+;;; License: MIT
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of
+;; the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
+;;
+;;; Commentary:
+;;
+;; To use it, add this file to a directory in your load-path, then put
+;; the following in your Emacs configuration file:
+;;
+;;   (load-theme 'autumn-light t)
 ;;
 ;;; Code:
 
